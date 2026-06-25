da0128733c0e1a9652dff3ad3bfc26ec7806ada5
The Anh <theanhtong022@gmail.com>
2026-06-25 20:12:38 +0700

    fix: load env with ConfigModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,18 @@ import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { BrandsModule } from './brands/brands.module.js';
 import { CategoriesModule } from './categories/categories.module.js';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module.js';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [BrandsModule, CategoriesModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    BrandsModule,
+    CategoriesModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
