d9a533a3061244e4daf554d0a7d0c84ff76daa34
The Anh <theanhtong022@gmail.com>
2026-06-25 19:58:23 +0700

    fix: add global PrismaModule for dependency injection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppService } from './app.service.js';
 import { BrandsModule } from './brands/brands.module.js';
 import { CategoriesModule } from './categories/categories.module.js';
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module.js';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [BrandsModule, CategoriesModule],
+  imports: [BrandsModule, CategoriesModule, PrismaModule],
 })
 export class AppModule {}
