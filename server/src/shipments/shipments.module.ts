d3b948b4446ce9ebdbf028284897d39d82e6b4d6
The Anh <theanhtong022@gmail.com>
2026-06-30 14:47:11 +0700

    refactor: remove dead code

--- server/src/shipments/shipments.module.ts
+++ server/src/shipments/shipments.module.ts
@@ -1,9 +1,7 @@
 import { Module, forwardRef } from '@nestjs/common';
 
 import { GhnMockProvider } from './providers/ghn/ghn-mock.provider.js';
-import { GhnProvider } from './providers/ghn/ghn.provider.js';
 import { GhnSandboxProvider } from './providers/ghn/ghn-sandbox.provider.js';
-import { GhnWebhookController } from './webhooks/ghn.controller.js';
 import { OrdersModule } from '../orders/orders.module.js';
 import { PaymentsModule } from '../payments/payments.module.js';
 import { SHIPPING_PROVIDER } from './interfaces/shipping-provider.interface.js';
@@ -12,8 +10,6 @@ import { ShipmentsService } from './shipments.service.js';
 
 const shippingProviderFactory = () => {
   switch (process.env.SHIPPING_PROVIDER) {
-    case 'ghn':
-      return new GhnProvider();
     case 'ghn-sandbox':
       return new GhnSandboxProvider();
     default:
@@ -23,7 +19,7 @@ const shippingProviderFactory = () => {
 
 @Module({
   imports: [forwardRef(() => OrdersModule), forwardRef(() => PaymentsModule)],
-  controllers: [ShipmentsController, GhnWebhookController],
+  controllers: [ShipmentsController],
   providers: [
     ShipmentsService,
     {
